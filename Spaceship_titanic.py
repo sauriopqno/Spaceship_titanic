f3c5ef90a2fb14f946539796caa22ac41024cf58
Imanol_elizondo_perucich <123278003+sauriopqno@users.noreply.github.com>
2024-08-14 20:53:38 -0600

    Spaceship_titanic.py

--- Spaceship_titanic.py
+++ Spaceship_titanic.py
@@ -2,7 +2,7 @@
 # spaceship titanic with keras and deep learning 
 # Application to the kaggle competition by Imanol Elizondo Perucich.
 
-# %%
+
 #import libraries
 import tensorflow as tf 
 import numpy as np
@@ -10,14 +10,13 @@ from tensorflow import keras
 import pandas as pd
 from sklearn import preprocessing
 
-# %%
 #read the dataset with pandas
 data=pd.read_csv("C:\\Users\\elizo\\Desktop\\proyectos\\spacetitan\\train.csv") #path to files
 test=pd.read_csv("C:\\Users\\elizo\\Desktop\\proyectos\\spacetitan\\test.csv") 
 testid=test["PassengerId"] #save this column
 
 
-# %%
+
 #eliminate the name column and fill in the missing values
 def clean(data):
     data=data.drop(["Name"], axis=1)
@@ -34,7 +33,7 @@ def clean(data):
 
 
 
-# %%
+
 # The cabin column has 3 pieces of data: deck, number and side, so we created an individual column for each.
 def cabin(dat):
     a=[]
@@ -51,7 +50,7 @@ def cabin(dat):
     
 
 
-# %%
+
 #The second part of the passengerid column gives us the number of people within a group so we extract the ones that are together in another column
 def pasid(dat):
     x=[]
@@ -67,7 +66,7 @@ def pasid(dat):
     dat["family"]=x
     return dat
 
-# %%
+
 #now we extract the number of people in each grup
 def group(dat):
     x=[]
@@ -77,7 +76,7 @@ def group(dat):
     dat["groupn"]=x
     return dat
 
-# %%
+
 #we run the functions for the test and data 
 data=clean(data)
 test=clean(test)
@@ -96,7 +95,7 @@ test=test.drop(["Cabin"], axis=1)
 
 
 
-# %%
+
 #We transform text data into numbers
 le=preprocessing.LabelEncoder()
 cols=["HomePlanet","CryoSleep","Destination","VIP","cabin0","cabin2"]
@@ -107,21 +106,17 @@ for col in cols:
 data.head()
     
 
-# %%
+
 data["Transported"]=le.fit_transform(data["Transported"])
 
-# %%
+
 #we select our y and x and transform them into numpy arrays 
 y =data ["Transported"]
 x= data.drop("Transported",axis=1)
 x = np.asarray(x).astype(np.float32)
 y = np.asarray(y).astype(np.float32)
- 
 
-# %%
-x.shape
 
-# %%
 #we build our model with layers
 model=tf.keras.Sequential([
     keras.layers.InputLayer(input_shape=(15)),
@@ -132,22 +127,18 @@ model=tf.keras.Sequential([
     keras.layers.Dropout(0.3),  # Dropout regularization
     
     keras.layers.Dense(1, activation='sigmoid')
-    
-    
-    
     ])
-#we compile our model 
+
 model.compile(optimizer='adam', loss="binary_crossentropy", metrics=['accuracy'])
 
 
-# %%
+
 model.summary()
 
-# %%
 #we train our model with a 20% validation split and 200 epochs
 model.fit(x,y,validation_split=.2, epochs=200 ,batch_size=32, verbose=1)
 
-# %%
+
 #transform the test data into a numpy array and predict the results 
 test = np.asarray(test).astype(np.float32)
 pred=model.predict(test)
